feb67dfcfc9d372891183143109b07a4d76d3c58
David Horvath <s38nbr@scl000009198.sccloud.swissre.com>
2018-10-23 16:15:38 +0200

    quick fix for symlink precondition; ensure ~/.bashrc runs setup_dotfiles.sh

--- happy_bashrc.sh
+++ happy_bashrc.sh
@@ -2,3 +2,4 @@
 
 ABS_ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"; pwd )"
 git --work-tree "$ABS_ROOT_DIR" --git-dir "$ABS_ROOT_DIR/.git" pull origin master
+$ABS_ROOT_DIR/scripts/setup_dotfiles.sh
--- scripts/setup_dotfiles.sh
+++ scripts/setup_dotfiles.sh
@@ -3,9 +3,9 @@
 ABS_ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.."; pwd )"
 for FILE in .vimrc
 do
-  if [ -f "~/$FILE" ]; then
-    echo "Skipping $FILE as it is an existing file"
-  elif [ ! -L $FILE ]; then
+  if [ -f ~/$FILE ] && [ ! -L ~/$FILE ] ; then
+    echo "WARNING: Skipping $FILE as it is an existing file and not a link"
+  elif [ ! -L ~/$FILE ]; then
     echo "LINK: ~/$FILE -> $ABS_ROOT_DIR/$FILE"
     ln -s "$ABS_ROOT_DIR/$FILE" ~/$FILE
   fi
