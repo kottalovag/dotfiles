469197d6e36ead6cd7ffec77a50333e8b2b7bd14
David Horvath <s38nbr@scl000009198.sccloud.swissre.com>
2018-10-23 17:06:57 +0200

    fix pull

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -3,5 +3,5 @@
 # This file is called from ../happy_bashrc.sh
 # This file should not be modified as it is updating other files: it should not pull the rug from under its own feet
 
-ABS_ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"; pwd )"
+ABS_ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.."; pwd )"
 git --work-tree "$ABS_ROOT_DIR" --git-dir "$ABS_ROOT_DIR/.git" pull origin master
