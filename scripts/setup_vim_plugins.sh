954c5e34d5b682153a187885f9da21d7112ccd65
David Horvath <s38nbr@scl000009198.sccloud.swissre.com>
2018-10-29 14:46:38 +0100

    fix rename plugins location

--- scripts/setup_vim_plugins.sh
+++ scripts/setup_vim_plugins.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 ABS_ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.."; pwd )"
-mkdir -p ~/.vim
+mkdir -p ~/.vim/plugin
 
 for FILE in Rename.vim
 do
-    target_plugin=~/.vim/$FILE
+    target_plugin=~/.vim/plugin/$FILE
     if [ ! -f $target_plugin ] && [ ! -L $target_plugin ] ; then
-        echo "LINK: $target_plugin -> $ABS_ROOT_DIR/.vim/$FILE"
-        ln -s "$ABS_ROOT_DIR/.vim/$FILE" $target_plugin
+        echo "LINK: $target_plugin -> $ABS_ROOT_DIR/.vim/plugin/$FILE"
+        ln -s "$ABS_ROOT_DIR/.vim/plugin/$FILE" $target_plugin
     fi
 done
