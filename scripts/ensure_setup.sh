449e886f208fdd5d7c7f40e06bff56191c8d2a66
David Horvath <s38nbr@scl000009198.sccloud.swissre.com>
2018-10-31 16:11:21 +0100

    read place of readme from git remote origin

--- scripts/ensure_setup.sh
+++ scripts/ensure_setup.sh
@@ -5,4 +5,4 @@ $ABS_ROOT_DIR/scripts/setup_bashrc.sh
 $ABS_ROOT_DIR/scripts/setup_dotfiles.sh
 $ABS_ROOT_DIR/scripts/setup_vim_plugins.sh
 
-echo "happy setup done, see details at https://github.com/kottalovag/happy_setup"
+echo "happy setup done, see details at $(git --work-tree "$ABS_ROOT_DIR" --git-dir "$ABS_ROOT_DIR/.git" remote get-url origin)"
