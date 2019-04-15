19508af6991a42cf33f78180364b6ce7d1974ccc
U-CORP\S38NBR <s38nbr@HUB50303.corp.gwpnet.com>
2019-04-15 17:44:11 +0200

    only sync once a day

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -4,4 +4,19 @@
 # This file should not be modified as it is updating other files: it should not pull the rug from under its own feet
 
 ABS_ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.."; pwd )"
-git --work-tree "$ABS_ROOT_DIR" --git-dir "$ABS_ROOT_DIR/.git" pull origin master
+TOUCHED="$ABS_ROOT_DIR/repo_touched"
+
+CURRENT_SECONDS=$(date +%s)
+if [ -f $TOUCHED ]; then
+    LAST_TOUCH_SECONDS=$(date +%s -r $TOUCHED)
+else
+    LAST_TOUCH_SECONDS=0
+fi
+
+#only pull once a day
+if [ "$((CURRENT_SECONDS - LAST_TOUCH_SECONDS))" -gt "$((60*60*24))" ]; then
+    git --work-tree "$ABS_ROOT_DIR" --git-dir "$ABS_ROOT_DIR/.git" pull origin master
+    touch $TOUCHED
+else
+    echo "Already synced today, won't pull"
+fi
