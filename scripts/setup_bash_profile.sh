8b94a894c8b7ebf53a6bf69f0a80f376cd5fc46f
David Horvath <David_Horvath@rcomext.com>
2019-04-16 11:11:41 +0200

    make sure we don't mess up distros without user bash_profile; fix exec bit on shell files

--- scripts/setup_bash_profile.sh
+++ scripts/setup_bash_profile.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-ABS_ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.."; pwd )"
-LINE=". $ABS_ROOT_DIR/happy_bash_profile.sh"
-grep -qF "$LINE" ~/.bash_profile || ( echo "including this in ~/.bash_profile:" && echo $LINE && echo "$LINE" >> ~/.bash_profile )
+if [ -f ~/.bash_profile ]; then
+    ABS_ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.."; pwd )"
+    LINE=". $ABS_ROOT_DIR/happy_bash_profile.sh"
+    grep -qF "$LINE" ~/.bash_profile || ( echo "including this in ~/.bash_profile:" && echo $LINE && echo "$LINE" >> ~/.bash_profile )
+fi
