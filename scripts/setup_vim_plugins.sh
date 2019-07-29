4d61800a3ab4e343474381afc3923aedb06f7c81
David Horvath <David_Horvath@rcomext.com>
2019-07-29 14:34:50 +0200

    add vim-ps1 plugin based on https://vimawesome.com/plugin/vim-ps1

--- scripts/setup_vim_plugins.sh
+++ scripts/setup_vim_plugins.sh
@@ -3,6 +3,8 @@
 ABS_ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.."; pwd )"
 mkdir -p ~/.vim/plugin
 mkdir -p ~/.vim/autoload
+mkdir -p ~/.vim/bundle
+
 
 for FILE in plugin/Rename.vim autoload/pathogen.vim
 do
@@ -10,5 +12,17 @@ do
     if [ ! -f $target_plugin ] && [ ! -L $target_plugin ] ; then
         echo "LINK: $target_plugin -> $ABS_ROOT_DIR/.vim/$FILE"
         ln -s "$ABS_ROOT_DIR/.vim/$FILE" $target_plugin
+    fi
+done
+
+repos=(
+    "bundle/vim-ps1,https://github.com/PProvost/vim-ps1.git"
+)
+for repo in ${repos[@]}; do
+    IFS=',' read SUBPATH GITURL <<< "${bundle}"
+    if [ ! -d ~/.vim/$SUBPATH ] && [ ! -L ~/.vim/$SUBPATH ] ; then
+        cd ~/.vim/bundle
+        git clone $GITURL
+        cd -
     fi
 done
